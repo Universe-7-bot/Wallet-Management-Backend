cddb705374346c07d8117d039dc73d5129a0d3b3
Sohan Singhamahapatra <sohansinghamahapatra02103@gmail.com>
2025-05-15 20:26:14 +0530

    fix: cors issue

--- server.js
+++ server.js
@@ -11,7 +11,24 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 connectDB();
 
-app.use(cors());
+// app.use(cors());
+
+// FOR DEPLOYMENT
+const allowedOrigins = ["https://cypherwallet.netlify.app"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Routes
